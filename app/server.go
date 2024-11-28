89753f6c56bf6cd9d0c141849d1e580fd208d93a
trieutrng <trieutrng@gmail.com>
2024-11-28 20:20:45 +0700

    stage 1: handle concurrent clients

--- app/server.go
+++ app/server.go
@@ -22,12 +22,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) error {
 	buf := make([]byte, 1024)
 	for {
 		read, err := conn.Read(buf)
