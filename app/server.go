fa5487f3084cd75765301a12a54346c476913310
trieutrng <trieutrng@gmail.com>
2024-12-05 06:56:08 +0700

    feat: configure listening port

--- app/server.go
+++ app/server.go
@@ -10,22 +10,39 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+type serverOption struct {
+	port string
+}
+
+func getServerOptions(args []string) serverOption {
+	opts := serverOption{
+		port: "6379",
+	}
+	for i, arg := range args {
+		if arg == "--port" {
+			if i < len(args)-1 {
+				opts.port = args[i+1]
+			}
+		}
+	}
+	return opts
+}
+
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	opts := getServerOptions(os.Args)
+
+	// Uncomment this block to pass the first stage
+	//
+	l, err := net.Listen("tcp", fmt.Sprintf(":%v", opts.port))
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
 
 	// init dependencies
 	respParser := NewRESP()
 	memory := NewMemory()
 	processor := NewProcessor(respParser, memory)
-
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
 
 	for {
 		conn, err := l.Accept()
