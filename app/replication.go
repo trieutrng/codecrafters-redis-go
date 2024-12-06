1eae99098f3a10e3b2391524f829752edd5daa2f
trieutrng <trieutrng@gmail.com>
2024-12-06 08:03:55 +0700

    feat: send handshake 2/3

--- app/replication.go
+++ app/replication.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"strings"
 	"time"
@@ -33,26 +34,116 @@ func InitReplication(procesor *Processor, opts serverOption) error {
 
 	if len(opts.replicaOf) > 0 {
 		ReplicationServerInfo.Role = "slave"
-
-		masterAddr := strings.Split(opts.replicaOf, " ")
-		conn, err := connectMaster(masterAddr[0], masterAddr[1])
+		err := handshake(procesor, opts)
 		if err != nil {
 			return err
 		}
-
-		pingResp := &RESP{
-			Type: Arrays,
-			Nested: []*RESP{
-				{
-					Type: BulkString,
-					Data: []byte("PING"),
-				},
-			},
-		}
-		pingMsg := procesor.parser.Serialize(pingResp)
-		conn.Write(pingMsg)
 	}
 	return nil
+}
+
+func handshake(procesor *Processor, opts serverOption) error {
+	masterAddr := strings.Split(opts.replicaOf, " ")
+	conn, err := connectMaster(masterAddr[0], masterAddr[1])
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// step 1: PING the master
+	// -> PING
+	pingResp := &RESP{
+		Type: Arrays,
+		Nested: []*RESP{
+			{
+				Type: BulkString,
+				Data: []byte("PING"),
+			},
+		},
+	}
+	pingMsg := procesor.parser.Serialize(pingResp)
+	fmt.Printf("SLAVE_REQUEST: %v", string(pingMsg))
+
+	data, err := request(conn, pingMsg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("MASTER_RESPOND: %v", string(data))
+
+	// step 2: send REPLCONF with slave address
+	// REPLCONF listening-port <PORT>
+	replConfAddr := &RESP{
+		Type: Arrays,
+		Nested: []*RESP{
+			{
+				Type: BulkString,
+				Data: []byte("REPLCONF"),
+			},
+			{
+				Type: BulkString,
+				Data: []byte("listening-port"),
+			},
+			{
+				Type: BulkString,
+				Data: []byte(opts.port),
+			},
+		},
+	}
+	replConfAddrMsg := procesor.parser.Serialize(replConfAddr)
+	fmt.Printf("SLAVE_REQUEST: %v", string(replConfAddrMsg))
+
+	data, err = request(conn, replConfAddrMsg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("MASTER_RESPOND: %v", string(data))
+
+	// step 3: send REPLCONF with capa psync
+	// -> REPLCONF capa psync 2
+	replConfPsync := &RESP{
+		Type: Arrays,
+		Nested: []*RESP{
+			{
+				Type: BulkString,
+				Data: []byte("REPLCONF"),
+			},
+			{
+				Type: BulkString,
+				Data: []byte("capa"),
+			},
+			{
+				Type: BulkString,
+				Data: []byte("psync2"),
+			},
+		},
+	}
+	replConfPsycnMsg := procesor.parser.Serialize(replConfPsync)
+	fmt.Printf("SLAVE_REQUEST: %v", string(replConfPsycnMsg))
+
+	data, err = request(conn, replConfPsycnMsg)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("MASTER_RESPOND: %v", string(data))
+
+	return nil
+}
+
+func request(conn net.Conn, data []byte) ([]byte, error) {
+	// send request
+	_, err := conn.Write(data)
+	if err != nil {
+		return nil, err
+	}
+
+	// waiting for the response
+	buf := make([]byte, 4096)
+	_, err = conn.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf, nil
 }
 
 func connectMaster(host, port string) (net.Conn, error) {
