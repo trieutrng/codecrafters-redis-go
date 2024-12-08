5201ebb9eed749f014f82f54bc9b317255292735
trieutrng <trieutrng@gmail.com>
2024-12-08 21:52:23 +0700

    feat: streams - blocking reads

--- app/processor.go
+++ app/processor.go
@@ -266,7 +266,7 @@ func xrange(memory *Memory) Executor {
 			Nested: make([]*RESP, 0),
 		}
 		stream := (entry.Value).(StreamEntry)
-		keyRange := QueryStreamKeysByRange(stream, start, end)
+		keyRange := QueryStreamKeysByRange(stream, start, end, true)
 
 		for _, key := range keyRange {
 			// for the item key
@@ -313,11 +313,31 @@ func xread(memory *Memory) Executor {
 			return nil, fmt.Errorf("insufficient arguments for XREAD")
 		}
 
-		streams := make(map[string]string)
-		numStream := int((len(resp.Nested) - 2) / 2)
+		// process options
+		isBlocking, blockingTime := false, 0
+		i := 1
+		for i < len(resp.Nested) {
+			if string(resp.Nested[i].Data) == "streams" {
+				i += 1
+				break
+			}
+			if string(resp.Nested[i].Data) == "block" {
+				isBlocking = true
+				blockingTime, _ = strconv.Atoi(string(resp.Nested[i+1].Data))
+				i += 2
+				continue
+			}
+			i += 1
+		}
+		if isBlocking {
+			<-time.After(time.Duration(blockingTime) * time.Millisecond)
+		}
 
-		for i := 2; i < len(resp.Nested)-numStream; i++ {
-			streams[string(resp.Nested[i].Data)] = string(resp.Nested[i+numStream].Data)
+		streams := make(map[string]string)
+		numStream := int((len(resp.Nested) - i) / 2)
+
+		for j := i; j < len(resp.Nested)-numStream; j++ {
+			streams[string(resp.Nested[j].Data)] = string(resp.Nested[j+numStream].Data)
 		}
 
 		output := &RESP{
@@ -329,7 +349,7 @@ func xread(memory *Memory) Executor {
 		for streamId, boundId := range streams {
 			entry := memory.Get(streamId)
 			stream := (entry.Value).(StreamEntry)
-			keyRange := QueryStreamKeysByRange(stream, boundId, "+")
+			keyRange := QueryStreamKeysByRange(stream, boundId, "+", false)
 
 			streamItemResp := &RESP{
 				Type:   Arrays,
@@ -383,8 +403,16 @@ func xread(memory *Memory) Executor {
 					streamItemResp,
 				},
 			}
+			if len(streamItemResp.Nested) > 0 {
+				output.Nested = append(output.Nested, streamQueryResp)
+			}
+		}
 
-			output.Nested = append(output.Nested, streamQueryResp)
+		if len(output.Nested) == 0 {
+			return &RESP{
+				Type: BulkString,
+				Data: []byte(""),
+			}, nil
 		}
 
 		return output, nil
